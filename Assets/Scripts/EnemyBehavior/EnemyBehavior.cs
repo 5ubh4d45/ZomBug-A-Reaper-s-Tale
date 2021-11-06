87c8a6681e2ce9717ae1460152e200831b558d1d
Subha Das <65977621+5ubh4d45@users.noreply.github.com>
2021-11-07 03:30:36 +0530

    updated enemy behaviour

--- Assets/Scripts/EnemyBehavior/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior/EnemyBehavior.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBehavior : MonoBehaviour
+namespace EnemyBehavior
 {
-    // Start is called before the first frame update
-    void Start()
+    public class EnemyBehavior : MonoBehaviour
     {
-        
-    }
+        [Header("Required Components")]
+        [SerializeField] private EnemyPathFinder enemyPathFinder;
+        [SerializeField] private Transform playerTarget;
+        [SerializeField] private Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private enum EnemyState
+        {
+            FreeRoam,
+            ChasingTarget,
+            AttackingTarget,
         
+        }
+        public bool canFollow = true;
+
+        // Start is called before the first frame update
+        void Start()
+        {   
+            if (enemyPathFinder == null){
+                enemyPathFinder = GetComponent<EnemyPathFinder>();
+            }
+            if (rb == null){
+                rb = GetComponent<Rigidbody2D>();
+            }
+            if (playerTarget == null){
+                playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            enemyPathFinder.FollowTarget(canFollow, playerTarget);
+        }
+    
+    
+    
     }
 }
--- Assets/Scripts/EnemyBehavior/EnemyPathFinder.cs
+++ Assets/Scripts/EnemyBehavior/EnemyPathFinder.cs
@@ -1,122 +1,117 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Pathfinding;
+using UnityEngine;
 
-public class EnemyPathFinder : MonoBehaviour
-{   
-    [Header("Required Components")]
-    [SerializeField] private Seeker seeker;
-    [SerializeField] private Rigidbody2D rb;
-
-    [Space]
-    [Header("PathFinding Attributes")]
-    [SerializeField] private float repeatRate = 0.5f;
-    [SerializeField] private float speed = 10f;
-    [SerializeField ]private float stopDistance = 1f;
-    [SerializeField] private float nextWaypointDistance = 2f;
-    
-    private Path path;
-    private int currentWaypoint = 0;
-    private bool reachedEndOfPath;
-    private float lastRepath = 0;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (seeker == null){
-            seeker = GetComponent<Seeker>();
-        }
-        if (rb == null){
-            rb = GetComponent<Rigidbody2D>();
-        }
-
-        
-    }
-
-    public void OnPathComplete(Path p){
-        
-        // Debug.Log("New Path was calculated. Did it failed with an error?" + p.error);
-
-        if (!p.error){
-            path = p;
-            //resets the currentWaypoint
-            currentWaypoint = 0;
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
+namespace EnemyBehavior
+{
+    public class EnemyPathFinder : MonoBehaviour
     {   
-        // FollowTarget(true, playerTarget);
-    }
+        [Header("Required Components")]
+        [SerializeField] private Seeker seeker;
+        [SerializeField] private Rigidbody2D rb;
 
-    public void FollowTarget(bool canFollow, Transform targetPosition){
+        [Space]
+        [Header("PathFinding Attributes")]
+        [SerializeField] private float repeatRate = 0.5f;
+        [SerializeField] private float speed = 10f;
+        [SerializeField ]private float stopDistance = 1f;
+        [SerializeField] private float nextWaypointDistance = 2f;
+    
+        private Path _path;
+        private int _currentWaypoint = 0;
+        private bool _reachedEndOfPath;
+        private float _lastRepath = 0;
 
-        if (!canFollow){
-            return;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            if (seeker == null){
+                seeker = GetComponent<Seeker>();
+            }
+            if (rb == null){
+                rb = GetComponent<Rigidbody2D>();
+            }
+
+        
         }
-        if (Time.time > lastRepath + repeatRate && seeker.IsDone()){
 
-            lastRepath = Time.time;
+        private void OnPathComplete(Path p){
+        
+            // Debug.Log("New Path was calculated. Did it failed with an error?" + p.error);
 
-            seeker.StartPath(rb.position, targetPosition.position, OnPathComplete);
-        }
-
-        if ( path == null){
-
-            return;
-        }
-
-        reachedEndOfPath = false;
-        float distanceToWaypoint;
-
-        while(true){
-            
-            //checks the distance
-            distanceToWaypoint = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
-
-            if (distanceToWaypoint < nextWaypointDistance){
-
-                //checks if we reached end or theres another waypont ahead
-                if (currentWaypoint + 1 < path.vectorPath.Count){
-                    currentWaypoint++;
-
-                } else{
-                
-                    reachedEndOfPath = true;
-                    break;
-                }
-            } else{
-
-                break;
+            if (!p.error){
+                _path = p;
+                //resets the currentWaypoint
+                _currentWaypoint = 0;
             }
         }
 
-        var speedFactor = reachedEndOfPath ? Mathf.Sqrt(distanceToWaypoint/nextWaypointDistance) : 1f;
+        public void FollowTarget(bool canFollow, Transform targetPosition){
 
-        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+            if (!canFollow){
+                return;
+            }
+            if (Time.time > _lastRepath + repeatRate && seeker.IsDone()){
 
-        Vector3 velocity = dir * speed * speedFactor;
+                _lastRepath = Time.time;
 
-        float distancefromTarget = Vector2.Distance(rb.position, targetPosition.position);
+                seeker.StartPath(rb.position, targetPosition.position, OnPathComplete);
+            }
 
-        if (distancefromTarget >= stopDistance){
+            if ( _path == null){
 
-            rb.position += (Vector2)velocity * Time.deltaTime;
+                return;
+            }
+
+            _reachedEndOfPath = false;
+            float distanceToWaypoint;
+
+            while(true){
+            
+                //checks the distance
+                distanceToWaypoint = Vector3.Distance(transform.position, _path.vectorPath[_currentWaypoint]);
+
+                if (distanceToWaypoint < nextWaypointDistance){
+
+                    //checks if we reached end or theres another waypoint ahead
+                    if (_currentWaypoint + 1 < _path.vectorPath.Count){
+                        _currentWaypoint++;
+
+                    } else{
+                
+                        _reachedEndOfPath = true;
+                        break;
+                    }
+                } else{
+
+                    break;
+                }
+            }
+
+            float speedFactor = _reachedEndOfPath ? Mathf.Sqrt(distanceToWaypoint/nextWaypointDistance) : 1f;
+
+            Vector3 dir = (_path.vectorPath[_currentWaypoint] - transform.position).normalized;
+
+            Vector3 velocity = dir * (speed * speedFactor);
+
+            float distancefromTarget = Vector2.Distance(rb.position, targetPosition.position);
+
+            if (distancefromTarget >= stopDistance){
+
+                rb.position += (Vector2)velocity * Time.deltaTime;
+            }
+
+
         }
 
+        public void OnDisable(){
+            seeker.pathCallback -= OnPathComplete;
+        }
 
-    }
-
-    public void OnDisable(){
-        seeker.pathCallback -= OnPathComplete;
-    }
-
-    private void OnDrawGizmosSelected() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(rb.position, stopDistance);
-        Gizmos.DrawWireSphere(rb.position, nextWaypointDistance);
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(rb.position, stopDistance);
+            Gizmos.DrawWireSphere(rb.position, nextWaypointDistance);
+        }
     }
 }
