cb62f0559c3afaf50798b0d8424f94416cf517e8
Amogh-Mittal <gamestudio275@gmail.com>
2021-11-19 10:59:57 +0530

    Musci Controller Null reference hot fix

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -17,27 +17,34 @@ namespace Game.Core
             //Get the emitter on this game object
             _emitter = GetComponent<StudioEventEmitter>();
 
-            //GameManager.Instance.OnGameStateChanged += UpdateMusic;
+            // Subscribe to the Game State changed event. 
+            // To make sure the emitter only plays when we are in the game scene.
+            GameManager.Instance.OnGameStateChanged += UpdateMusic;
         }
 
-        //private void OnDestroy()
-        //{
-        //    GameManager.Instance.OnGameStateChanged -= UpdateMusic;
-        //}
+        private void OnDestroy()
+        {
+            //Unsubscribe to the Game state changed event to avoid null reference exceptions
+            GameManager.Instance.OnGameStateChanged -= UpdateMusic;
+        }
 
-        //private void UpdateMusic()
-        //{
-        //    if (GameManager.Instance.GameState == GameState.GAME)
-        //    {
-        //        _emitter.Event = "event:/MUSIC_level1";
-        //        _emitter.Play();
-        //    }
-        //}
+        private void UpdateMusic()
+        {
+            if (GameManager.Instance.GameState == GameState.GAME)
+            {
+                //The new game state is the Game scene, so play the emitter.
+                _emitter.Play();
+            }
+        }
 
         // Update is called once per frame
         void Update()
         {
-            _emitter.SetParameter("player_health", Player.Instance.HealthSystem.HealthPercent);
+            if (GameManager.Instance.GameState == GameState.GAME)
+            {
+                // The game state is the Game Scene. which means the player health is available to us. so update it.
+                _emitter.SetParameter("player_health", Player.Instance.HealthSystem.HealthPercent);
+            }
         }
     }
 }
