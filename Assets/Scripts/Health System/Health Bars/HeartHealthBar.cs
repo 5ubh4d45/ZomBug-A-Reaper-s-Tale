0b7d32045b73f6c27683d67b06640edd153be8b1
Amogh-Mittal <gamestudio275@gmail.com>
2021-12-01 21:52:55 +0530

    Fix Heart Bar Sizing

--- Assets/Scripts/Health System/Health Bars/HeartHealthBar.cs
+++ Assets/Scripts/Health System/Health Bars/HeartHealthBar.cs
@@ -12,6 +12,8 @@ namespace Game.HealthSystem
         #region Variables
         [SerializeField] private Sprite[] _heartSprites;
         [SerializeField] private Color _heartColor;
+        [SerializeField] private float _heartSize;
+        [SerializeField] private Vector2 _referenceCanvasSize;
         private List<Image> _hearts;
         #endregion
 
@@ -42,8 +44,9 @@ namespace Game.HealthSystem
             Vector2 anchoredPosition = Vector2.zero;
             for (int i = 0; i < _healthSystem.HeartCount; i++)
             {
+                float heartSize = _heartSize / _referenceCanvasSize.x * Screen.width;
                 CreateHeartImage(anchoredPosition, i);
-                anchoredPosition += new Vector2(30, 0);
+                anchoredPosition += new Vector2(heartSize, 0);
             }
 
             // subscribe to new health system
@@ -68,27 +71,29 @@ namespace Game.HealthSystem
         private void HealthChanged(float changeAmount)
         {
             Debug.Log("Starting HealthChanged");
-            
+
             for (int i = 0; i < _healthSystem.HeartCount; i++)
             {
                 Heart heart = _healthSystem.Hearts[i];
                 _hearts[i].sprite = _heartSprites[heart.Fragments];
-                
+
                 Debug.Log("Health Changing:  " + heart.Fragments);
             }
-            
+
             Debug.Log("Ending HealthChanged");
         }
 
         private Image CreateHeartImage(Vector2 anchoredPosition, int index)
         {
             GameObject heartGameObject = new GameObject($"Heart {index}", typeof(Image));
+            float heartSizeX = _heartSize / _referenceCanvasSize.x * Screen.width;
+            float heartSizeY = _heartSize / _referenceCanvasSize.y * Screen.height;
 
             heartGameObject.transform.SetParent(transform);
             heartGameObject.transform.localPosition = Vector3.zero;
 
             heartGameObject.GetComponent<RectTransform>().anchoredPosition = anchoredPosition;
-            heartGameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(30, 30);
+            heartGameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(heartSizeX, heartSizeY);
 
             Image heartImage = heartGameObject.GetComponent<Image>();
             heartImage.sprite = _heartSprites[_heartSprites.Length - 1];
