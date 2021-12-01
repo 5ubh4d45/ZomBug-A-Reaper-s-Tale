98201109e6ad3e2c80798b579d8d6bdbae3acc62
JaneYWei <janewei.music@gmail.com>
2021-12-01 22:33:15 +1000

    Jane - catboss death sounds

--- Assets/Scripts/Boss/BossAnimator.cs
+++ Assets/Scripts/Boss/BossAnimator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class BossAnimator : MonoBehaviour
 {
@@ -63,6 +64,8 @@ public class BossAnimator : MonoBehaviour
 
     public void PlayDeathAniamtion()
     {
+        RuntimeManager.PlayOneShot(soundHolder.DeathSound);
+
         anim.SetTrigger("Death");
     }
 }
